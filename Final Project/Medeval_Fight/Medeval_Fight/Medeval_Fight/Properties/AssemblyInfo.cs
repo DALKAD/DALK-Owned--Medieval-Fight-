b3874bc0a17106d92a6544f1d96d86a8cea06a9f
dcharles525 <dcharles525@gmail.com>
2014-01-13 20:10:58 -0500

    Alpha 1.0.1
    Preparing for transfer to DALK;

--- Final Project/Medeval_Fight/Medeval_Fight/Medeval_Fight/Properties/AssemblyInfo.cs
+++ Final Project/Medeval_Fight/Medeval_Fight/Medeval_Fight/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Medeval_Fight")]
-[assembly: AssemblyProduct("Medeval_Fight")]
+[assembly: AssemblyTitle("Medieval Fight")]
+[assembly: AssemblyProduct("Medieval Fight")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
